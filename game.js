c21d15f4e44cef8143e982dcffd892ef0ac19c67
U-CORP\rrowe <rrowe@WIN-UEGN02P4K36.corp.amazonworkspaces.com>
2017-03-22 10:35:29 -0400

    Completed Javascript with comments

--- game.js
+++ game.js
@@ -1,18 +1,27 @@
+/* h1 Element targeted*/
 var h1 = document.getElementsByTagName("h1");
 
 var begin = Number(h1.innerHTML);
 
+/*targeting element by Id which effects
+ the number im playing too*/
+
 var game = document.getElementById("max");
 
+/* Declaring a variable to set it equal to 
+the targeted span Id in points1 and which is 
+later targeted*/
 var base = 0;
 
 
+/* Declaring a variable for the input 
+field to target its value */
 var box = document.getElementById("box");
 
 
-
-
-
+/* Declaring a variable and adding an event to
+the reset button as well as defining a function 
+for it */
 
 var reset = document.getElementById("reset");
 reset.addEventListener("click", Reset);
@@ -30,6 +39,10 @@ function Reset() {
 	}
 }
 
+/* Adding an event listener to variable box 
+and creating a function that causes variable 
+max content when the input field number is changed*/ 
+
 box.addEventListener("change", function() {
 	var end = 0
 	max.textContent = box.value;
@@ -41,16 +54,11 @@ box.addEventListener("change", function() {
 	if(points2.innerHTML == end) {
 	Reset();
 	}
-
-
-
 	
 });
 
-
-
- 
-
+/*Declaring a variable fot the span elements with id
+points that is going to be used to effect the score*/
 
 
 var points2 = document.getElementById("points2");
@@ -59,17 +67,12 @@ points2.innerHTML = base;
 var points1 = document.getElementById("points1");
 points1.innerHTML = base;
 
-var base = 0;
 
 
-
-
-/*var input = document.getElementsByTagName("input");
-input.addEventListener("change",function() {
-
-});*/
-
-
+/* Creating a function for both points to
+incremented if the 2 conditions are met 
+and once the points = box.value it changes 
+the color to green*/
 
 var p1 = document.getElementById("p1");
 p1.addEventListener("click",PlayerOne); 
@@ -80,8 +83,7 @@ function PlayerOne() {
 			points1.innerHTML++;
 		}
 	}
-			
-	
+		
 	if(points1.innerHTML == box.value){
 		points1.style.color = "green";
 	}
